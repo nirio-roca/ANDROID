9fb70f0c8021a3e8e7fa11bb6b97c16bd8fd2f09
SrNico <rocanirio@gmai.com>
2023-07-14 13:25:02 -0500

    Examen Promedio

---
+++ PROMEDIO/app/src/main/java/com/example/promedio/actFinal.kt
@@ -0,0 +1,39 @@
+package com.example.promedio
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+class actFinal : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_act_final)
+
+        val bundle = intent.extras;
+        val Usuario = bundle?.getString("Usuario").toString()
+        val Promedio = bundle?.getString("Promedio").toString()
+        val Estado = bundle?.getString("estado").toString()
+        val  lbluser  = findViewById<TextView>(R.id.lblNombre).apply {
+            text = Usuario;
+        }
+
+        val  lblnota  = findViewById<TextView>(R.id.lblpromedio).apply {
+            text = Promedio;
+        }
+        val  lblestado  = findViewById<TextView>(R.id.lblestado).apply {
+            text = Estado;
+        }
+
+
+    }
+
+    fun  regresarInicio(view: View) {
+        val regresar = Intent(this, MainActivity::class.java).apply {
+        }
+        startActivity(regresar);
+    }
+}
